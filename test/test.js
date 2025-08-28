3e56861cf2af6e787c36a3c8c132ea174f8c56c9
Benjamin Britton <120000566+bennyjoseph23@users.noreply.github.com>
2025-08-28 14:24:53 +0000

    Last commit

--- test/test.js
+++ test/test.js
@@ -1,8 +1,9 @@
 var main = require('../index');
 var assert = require('assert');
 describe('Regular fibonacci value', function() {
-    it('should return 5 for Fib(5)', function() {
+    it('should return 5 for Fib(5) and should return undef for fib(-2)', function() {
         assert.strictEqual(main.fibonacci(5), 5);
+        assert.strictEqual(main.fibonacci(-2), undefined);
       });
 });
 
@@ -10,5 +11,6 @@ describe('First two terms', function() {
   it('should return 0 and 1 for first two terms', function() {
       assert.strictEqual(main.fibonacci(0), 0);
       assert.strictEqual(main.fibonacci(1), 1);
+      
     });
 });
